c16b1a310c9dc794e43a3ff42da98754fccb2b41
Utkarsh-PinE <utkarsh.pine.2020@gmail.com>
2021-11-21 03:27:55 +0530

    Added comments and minor changes.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Guess The Number Game
+
+In this game you have to guess the number , on each guess to you get to know the count of right number of digits you have guessed and also the right location (if any.)
+
+For Example :
+Say, the right number is 2472 and you guessed the number as 1273,
+the output will be Right numbers : 2 and right location : 1 because
+2 and 7 are the right guess numbers but only 7 is on its right position.
+
+> Note : You will not be able to see the right numbers, only the count of right numbers.
+
+The challenge is to guess the numbers in the maximum possible moves.
--- main.sh
+++ main.sh
@@ -1,37 +1,48 @@
 #!/usr/bin/bash
 
-var=0
-# echo -n "_ _ _ _"
+#Generating a random 4-digit number
+actualnumber=$(($((RANDOM%9000))+1000))
 echo -en "_ _ _ _\033[7D"
-pos=1
-i=1
-while [[ true ]]
+var=0 pos=1 i=1
+while true
 do
-	if [ $pos -eq 5 ] && [ ${#arr[@]} -eq 4 ]
+	#Stop to take input if the 4th number is entered.
+	if [ $pos -eq 5 ] && [ ${#input[@]} -eq 4 ]
 	then
 		break
 	fi
-	read -sn1 var
+	read -sr -n1 var
 	case $var in
 	a|A)
+		#Moving cursor to left on pressing a/A.
 		echo -en "\033[2D"
+
+		#Not moving cursor if already at starting position.
 		if [ $pos -ge 2 ]
-		then 
-			let pos=pos-1;
+		then
+			((pos--))
 		fi;;
 	d|D)
-		if [ $pos -le ${#arr[@]} ]
+		#Moving cursor to right (only till last entered number)	on pressing d/D.
+		if [ $pos -le ${#input[@]} ]
 		then
 			echo -en "\033[2C"
-			let pos=pos+1;
-		fi
-		;;
+			((pos++))
+		fi;;
 	[0-9])
 		echo -n "$var "
-		arr[$pos]=$var
-		let pos=pos+1;
+
+		#Storing the input in an array.
+		input[$pos]=$var
+		((pos++))
 	esac
-	let i=i+1
+	((i++))
 done
 
-echo -e "\nArray : ${arr[@]}"
+#Converting the array into actual number.
+guessednumber=$(IFS=$'\0';echo "${input[*]}")
+if [ $actualnumber -eq "$guessednumber" ]; then
+	echo -e "\nSuccess!!!"
+else
+	echo -e "\nFail!!!"
+fi
